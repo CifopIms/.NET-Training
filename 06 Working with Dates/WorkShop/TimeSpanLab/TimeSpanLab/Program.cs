1b8fe597ba77c3e4801c30f98c3512dda2fd45a6
Ahmed HEDFI <ahmed.hedfi@gmail.com>
2022-09-16 21:01:48 +0100

    Day6

---
+++ 06 Working with Dates/WorkShop/TimeSpanLab/TimeSpanLab/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSpanLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
+
+            // Use Case
+            var orderCreateDate =new DateTime(2022,01,05);
+            var now = DateTime.Now;
+            var duration = now - orderCreateDate;
+            var date = DateTime.Now;
+            var anotherDate = date.AddMinutes(5);
+            if(duration > TimeSpan.FromDays(20) )
+            //if(duration >  new TimeSpan(20,0,0,0))
+            { 
+                // Send Alert Email...
+            }
+            Console.WriteLine("Duration :" + duration);
+
+            //Properties
+            Console.WriteLine("Minutes :" + timeSpan.Minutes);
+            Console.WriteLine("total Minutes :" + timeSpan.TotalMinutes);
+
+            //Add
+            timeSpan.Add(TimeSpan.FromMinutes(10));
+            Console.WriteLine("Add :" + timeSpan.Add(TimeSpan.FromMinutes(8)));
+           
+            Console.WriteLine("Substract :" + timeSpan.Subtract(TimeSpan.FromMinutes(10)));
+            
+            //ToString
+            Console.WriteLine("ToString :" + timeSpan.ToString());
+
+            var TimeSpanFromString = TimeSpan.Parse("01:02:03");
+            //Parse
+            Console.WriteLine("Parse :" + TimeSpan.Parse("01:02:03"));
+
+        }
+    }
+}
---
+++ 07 Working with Text/WorkShop/StringLabs/StringLabs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string value = null;
+            //Program x;
+            
+            //int number = Convert.ToInt32(value);
+            ////number = null;
+            //var number2 = int.Parse(value);
+            string fullName = "Ahmed Hedfi ";
+            //fullName = fullName.Trim();
+            Console.WriteLine("Trim : '{0}' Origin '{1}'", fullName.Trim(), fullName);
+            Console.WriteLine("ToUpper : '{0}'", fullName.Trim().ToUpper());
+            int index = fullName.IndexOf(' ');
+            string firstName = fullName.Substring(0, index);
+            string lastName = fullName.Substring(index + 1);
+            Console.WriteLine("firstName : '{0}'", firstName);
+            Console.WriteLine("lastName : '{0}'", lastName);
+            var replacedString = fullName.Replace("ahmed", "hello");
+            Console.WriteLine(fullName.Replace("Ahmed", "hello"));
+            string name = string.Empty;
+            if (string.IsNullOrEmpty(name))
+                Console.WriteLine("Invalid");
+            string str = "100";
+            decimal ammount = Convert.ToDecimal(str);
+            var formattedAmmount = ammount.ToString("C",new CultureInfo("it"));
+            var number = 123;
+            var fixDigits = number.ToString("D5");
+            Console.WriteLine(formattedAmmount);
+
+        }
+    }
+}
